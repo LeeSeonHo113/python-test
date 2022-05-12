e94416d43d0dcbba5cff1a6f54b861ecd106b668
a01048197443 <a01048197443@gmail.com>
2022-05-12 14:48:09 +0900

    빅데터 리템 개발

---
+++ src/test/java/site/metacoding/pythontest/PythonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.pythontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PythonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
